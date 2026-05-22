f0d2508a43f3d8b1e1223e6b0313e7a0c53d6aa6
qodirxoncoder <qodirxonm2003@gmail.com>
2026-05-23 01:03:11 +0500

    readmi fayli yangilandi

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ from users import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+#about.html fayli uchun view
+def about(request):
+    return render(request, 'about.html')
