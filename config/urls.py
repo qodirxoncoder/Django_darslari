2458c88237932fc2dd19a043d1a9c37adb89a65f
qodirxoncoder <qodirxonm2003@gmail.com>
2026-05-23 02:00:26 +0500

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from users import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', include('users.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from .models import User
+from django.shortcuts import redirect
+
 
 def index(request):
-    return render(request, 'index.html')
+    users = User.objects.all()
+    return render(request, 'index.html', {'users': users})
 
 #about.html fayli uchun view
 def about(request):
     return render(request, 'about.html')
+
+#add_user.html fayli uchun view
+def add_user(request):
+    return render(request, 'add_user.html')
+
+
+#add_user.html faylidan malumotlarni qabul qilib, bazaga saqlash uchun view
+def add_user(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        last_name = request.POST['last_name']
+        phone_number = request.POST['phone_number']
+        User.objects.create(name=name, last_name=last_name, phone_number=phone_number)
+        return redirect('index')
+    return render(request, 'add_user.html')
